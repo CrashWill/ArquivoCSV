8db1ab041dd4ac0aab44faaab609ad1e50d84308
William Cavasotto <pioneirowill@gmail.com>
2017-10-11 20:53:09 -0300

    Criação do Arquivo CSV

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.IO;
 
 namespace ArquivoCSV
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+     {
+            
+            string nome, email;
+            int idade;
+            Console.WriteLine("Digite o seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu email: ");
+            email = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade:");
+            idade = Int16.Parse(Console.ReadLine());
+
+            StreamWriter arquivo = new StreamWriter("dados.csv",true);
+            arquivo.WriteLine(nome+";"+email+";"+idade+";"+DateTime.Now.ToShortDateString());
+            arquivo.Close();
+
+
+
+
+
+
+
+       
         }
     }
 }
